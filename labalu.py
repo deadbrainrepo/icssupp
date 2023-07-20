fe31923cde6d3e29589a403609b2a81b1958dba1
texnodev <140055201+texnodev@users.noreply.github.com>
2023-07-20 12:08:01 +0300

    dobavil zalupu

--- labalu.py
+++ labalu.py
@@ -1,11 +1,12 @@
 import subprocess
-
+cypher = (1)
 print("Здравствуйте!")
 print("Вас приветствует диагностическая утилита ICSSUPP.")
 print("Чем могу помочь?")
-print("1) Проверить состояние служб\n2) Послать нахуй\n")
-cypher = input()
-if cypher == "1":
-    print("Запускаю...")
-    subprocess.call("/usr/local/ics/support/bin/status")
-else: print("Иди нахуй")
+while cypher == (1) or (2):
+    print("1) Проверить состояние служб\n2) Послать нахуй\n")
+    cypher = input()
+
+    if cypher == "1":
+        subprocess.call("/usr/local/ics/support/bin/status")
+    else: print("Иди нахуй")
