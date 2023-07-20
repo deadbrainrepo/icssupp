fd3b3c8062aba79f57e2c287540420fad983bd24
texnodev <140055201+texnodev@users.noreply.github.com>
2023-07-20 19:36:15 +0300

    dobavil zalupu

--- color.py
+++ color.py
@@ -83,7 +83,8 @@ def print_pepe_ascii_art_start():
 bye = 'Бывай, начальник!\n'
 run = '......\n'
 hi = '----------------------------------\nЗдравствуйте!\nВас приветствует диагностическая утилита ICSSUPP.\nЧем могу помочь?\n----------------------------------'
-print_pepe_ascii_art_start() = (pepe_h)
+pepe_h = print_pepe_ascii_art_start
+pepe_q = pepe_h
 green("yap")
 subprocess.call("clear")
 #print_pepe_ascii_art_start()
@@ -92,7 +93,7 @@ for i in run:
     print(i, end='', flush=True)
 subprocess.call("clear")
 
-print(pepe_h)
+print(pepe_q)
 
 subprocess.call("clear")
 
