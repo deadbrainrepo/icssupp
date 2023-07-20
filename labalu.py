530d94ff47fd00d7e3e96e2ba4ccff84550122c6
texnodev <140055201+texnodev@users.noreply.github.com>
2023-07-20 12:21:36 +0300

    dobavil zalupu

--- labalu.py
+++ labalu.py
@@ -4,12 +4,13 @@ print("Здравствуйте!")
 print("Вас приветствует диагностическая утилита ICSSUPP.")
 print("Чем могу помочь?")
 while cypher != 0:
-    print("1) Проверить состояние служб\n2) Послать нахуй\n")
+    print("1) Проверить состояние служб\n2) Послать нахуй\n3)Запустить ребилд")
     cypher = input()
 
     if cypher == "1":
         subprocess.call("/usr/local/ics/support/bin/status")
     if cypher == "2":
         print("Иди нахуй")
+        subprocess.call("^ C")
     if cypher == "3":
         subprocess.call("/usr/local/ics/backup/bin/rebuild")
