896197f4f3ceed58b3d718ecce38db3015014048
texnodev <140055201+texnodev@users.noreply.github.com>
2023-07-20 15:05:58 +0300

    dobavil zalupu

--- anek.py
+++ anek.py
@@ -56,31 +56,35 @@ print("Вас приветствует диагностическая утили
 print("Чем могу помочь?")
 
 while True:
-    print("1) Проверить состояние служб\n2) Послать нахуй\n3) Сделать ребилд\n4) Починить ClamAV\n5) Проверить стейты\n6) Продлить тестовый период\n7) Пошутить\n0) Выход")
+    print("1) Проверить состояние служб\n2) Сделать ребилд\n3) Починить ClamAV\n4) Проверить "
+          "стейты\n5) Продлить тестовый период\n6) Пошутить\n0) Послать на хуй")
     cypher = input()
 
-    if cypher == "1":
-        subprocess.call("/usr/local/ics/support/bin/status")
-    elif cypher == "2":
+    if cypher == "0":
         print("Иди нахуй, другалёк")
         print_pepe_ascii_art()
         break
-    elif cypher == "3":
+    elif cypher == "1":
+        subprocess.call("/usr/local/ics/support/bin/status")
+
+    elif cypher == "2":
         subprocess.call("/usr/local/ics/backup/bin/rebuild")
-    elif cypher == "4":
+    elif cypher == "3":
         subprocess.call("xs clamav disable", shell=True)
         subprocess.call("rm -rf /var/db/clamav/*", shell=True)
         subprocess.call("freshclam", shell=True)
         subprocess.call("xs clamav enable", shell=True)
-    elif cypher == "5":
+    elif cypher == "4":
         print("----------------------------------")
         subprocess.call("pfctl -ss | sed 's/\:/ /g' | awk '{print $(NF-3)}' | sort -f | uniq -c | sort -k 1nr -k 2f | head", shell=True)
         print("----------------------------------")
-    elif cypher == "6":
+    elif cypher == "5":
         subprocess.call("sh -c 'BASE_PARTITION=\"'$(zfs list -d 0 -H | cut -f 1 | grep zp)'\" && zfs set zfs:trial=839 $BASE_PARTITION@ok' && xs execworker restart && xs jojoba restart", shell=True)
-    elif cypher == "7":
+    elif cypher == "6":
+        print("----------------------------------")
         print(get_random_joke())
-    elif cypher == "0":
-        break
+        print("----------------------------------")
     else:
+        print("----------------------------------")
         print("Нет такой буквы")
+        print("----------------------------------")
